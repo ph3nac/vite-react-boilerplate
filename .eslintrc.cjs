5fb0c81ce56381c5d73aa6dcce37957c7110ea5e
ph3 <100564304+ph3nac@users.noreply.github.com>
2022-11-11 19:22:40 +0900

    cmd: npx sb init --builder @storybook/builder-vite

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,32 +1,23 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true,
+    es2021: true
   },
-  extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'airbnb',
-    'airbnb/hooks',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:import/errors',
-    'plugin:import/warnings',
-    'plugin:import/typescript',
-    'plugin:@typescrpt-eslint/recommended-requiring-type-checking',
-    'prettier',
-  ],
+  extends: ['eslint:recommended', 'plugin:react/recommended', 'airbnb', 'airbnb/hooks', 'plugin:@typescript-eslint/recommended', 'plugin:import/errors', 'plugin:import/warnings', 'plugin:import/typescript', 'plugin:@typescrpt-eslint/recommended-requiring-type-checking', 'prettier', 'plugin:storybook/recommended'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
-      jsx: true,
+      jsx: true
     },
     ecmaVersion: 'latest',
     sourceType: 'module',
-    project: './tsconfig.json',
+    project: './tsconfig.json'
   },
   plugins: ['react', 'react-hooks', '@typescript-eslint', 'jsx-a11y', 'import'],
   root: true,
   rules: {
-    'react/no-unknown-property': ['error', { ignore: ['css'] }],
-  },
+    'react/no-unknown-property': ['error', {
+      ignore: ['css']
+    }]
+  }
 };
