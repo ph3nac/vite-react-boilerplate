07234d35e9ec48bf43840885e895289d65b67a72
ph3 <100564304+ph3nac@users.noreply.github.com>
2022-11-13 20:22:31 +0900

    edited: vite.config.ts .storybook/main.cjs

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -1,18 +1,41 @@
+const react = require('@vitejs/plugin-react');
+
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
   ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
-  ],
-  "framework": "@storybook/react",
-  "core": {
-    "builder": "@storybook/builder-vite"
+  framework: '@storybook/react',
+  core: {
+    builder: '@storybook/builder-vite',
   },
-  "features": {
-    "storyStoreV7": true
-  }
-}
+  features: {
+    storyStoreV7: true,
+  },
+  async viteFinal(config) {
+    config.plugins = config.plugins.filter(
+      (plugin) =>
+        !(Array.isArray(plugin) && plugin[0]?.name.includes('vite:react')),
+    );
+
+    config.plugins.push(
+      react({
+        exclude: [/\.stories\.(t|j)sx?$/, /node_modules/],
+        jsxImportSource: '@emotion/react',
+        babel: {
+          plugins: ['@emotion/babel-plugin'],
+        },
+      }),
+    );
+
+    config.esbuild = {
+      // Fixed: [vite] warning: Top-level "this" will be replaced with undefined since this file is an ECMAScript module
+      // https://github.com/vitejs/vite/issues/8644
+      logOverride: { 'this-is-undefined-in-esm': 'silent' },
+    };
+
+    return config;
+  },
+};
